3523869432b322825a59678469385822577ceb75
Ryu <78216411+dongmen5149@users.noreply.github.com>
2022-01-12 01:38:49 +0900

    CRUD client단 1차완성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
 import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useState } from 'react';
+import ReactHtmlParser from 'react-html-parser';
 
 function App() {
+  const [movieContent, setMovieContent] = useState({
+    title: '',
+    content: ''
+  })
+
+  const [viewContent, setviewContent] = useState([])
+
+  const getValue = e => {
+    const { name, value } = e.target;
+    setMovieContent({
+      ...movieContent,
+      [name]: value
+    })
+    console.log(movieContent);
+  };
+
   return (
     <div className="App">
       <h1>Movie Review</h1>
       <div className='movie-container'>
-        <h2>제목</h2>
+        <h2>{element.title}</h2>
         <div>
-          내용
+          {ReactHtmlParser(element.content)}
         </div>
       </div>
       <div className='form-wrapper'>
-        <input className="title-input" type='text' placeholder='제목' />
-        <textarea className="text-area" placeholder='내용'></textarea>
+        <input className="title-input"
+          type='text'
+          placeholder='제목'
+          onChange={getValue}
+          name='title'
+        />
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+            setMovieContent({
+              ...movieContent,
+              content: data
+            })
+            console.log(movieContent);
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
       </div>
-      <button className="submit-button">입력</button>
+      <button className="submit-button"
+        onClick={() => {
+          setviewContent(viewContent.concat({ ...movieContent }))
+        }}>입력</button>
     </div>
   );
 }
